81e0b0aba391fefe85461bdc5866e3071cc1ef99
rayui <49279418+Rayui1225@users.noreply.github.com>
2024-08-22 18:25:16 +0800

    Update store_test.go
    fix some error

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -60,8 +60,8 @@ func TestTransferTx(t *testing.T) {
 
 		toEntry := result.ToEntry
 		require.NotEmpty(t, toEntry)
-		require.Equal(t, account1.ID, toEntry.AccountID)
-		require.Equal(t, -amount, toEntry.Amount)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
 		require.NotZero(t, toEntry.ID)
 		require.NotZero(t, toEntry.CreatedAt)
 
@@ -80,7 +80,7 @@ func TestTransferTx(t *testing.T) {
 		//check accounts' balance
 		diff1 := account1.Balance - fromAccount.Balance
 		diff2 := account2.Balance - toAccount.Balance
-		require.Equal(t, diff2, diff1)
+		require.Equal(t, diff2, -diff1)
 		require.True(t, diff1 > 0)
 		require.True(t, diff1%amount == 0)
 
